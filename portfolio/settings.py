9b7207ebe5f50916730f3bef8070de7496f3067e
em <em@LAPTOP-27CJU0E4>
2020-07-04 13:07:44 +0530

    Added static files support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
+STATIC_ROOT = os.path.join(BASE_DIR,"..","www", "static")
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
