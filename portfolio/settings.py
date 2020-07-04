0f2f060d392d11bb5742a7514de373ada3a386e4
em <em@LAPTOP-27CJU0E4>
2020-07-04 14:22:09 +0530

    static file support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'e=#m9*-whq)zt$+-!vfsd45$=inzjtqpzbmw$55dxxtxhee_(r)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'eba-vetpszpj.us-west-2.elasticbeanstalk.com'
+]
 
 
 # Application definition
@@ -118,8 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,"..","www", "static")
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+# STATIC_ROOT = os.path.join(BASE_DIR,"..","www", "static")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles"),
